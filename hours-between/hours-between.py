a1653a4af74424fa7badff0e9b2e4ce92c6e5433
jak18015 <jkellerm@umich.edu>
2025-05-12 16:31:11 -0400

    initial

---
+++ hours-between/hours-between.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+def hours_between_dates(date1_str, date2_str, date_format="%Y-%m-%d-%H-%M"):
+    """
+    Calculates the number of hours between two dates given in 'YYYY-MM-DD-HH-MM' format.
+
+    Parameters:
+    - date1_str (str): The first date as a string.
+    - date2_str (str): The second date as a string.
+    - date_format (str): The format of the input date strings. Default is 'YYYY-MM-DD-HH-MM'.
+
+    Returns:
+    - float: The number of hours between the two dates.
+    """
+    try:
+        date1 = datetime.strptime(date1_str, date_format)
+        date2 = datetime.strptime(date2_str, date_format)
+        delta = abs(date2 - date1)
+        return delta.total_seconds() / 3600
+    except ValueError as e:
+        print(f"Error parsing dates: {e}")
+        return None
+
+date1 = "2025-05-11-12-30"
+date2 = "2025-05-12-10-30"
+
+hours = hours_between_dates(date1, date2)
+print(f"Hours between dates: {hours}")
