297cfa662258b4a6d22fb54a26a0eeda03083607
jak18015 <jkellerm@umich.edu>
2025-06-23 15:05:26 -0400

    formaldehyde

--- c1v1/formaldehyde.py
+++ c1v1/formaldehyde.py
@@ -1,21 +1,24 @@
 from c1v1 import c1v1
 
-formaldehyde_stock = 10  # Stock solution concentration as a percentage (10%)
-formaldehyde_dilution = 4  # Desired final concentration as a percentage (4%)
+def formaldehyde():
+      formaldehyde_stock = 10  # Stock solution concentration as a percentage (10%)
+      formaldehyde_dilution = 4  # Desired final concentration as a percentage (4%)
 
-pbs_stock = 10 # Stock PBS solution as a multiple of 1x (10xPBS)
-pbs_dilution = 1  # Desired final concentration as a multiple of 1x (1xPBS)
+      pbs_stock = 10 # Stock PBS solution as a multiple of 1x (10xPBS)
+      pbs_dilution = 1  # Desired final concentration as a multiple of 1x (1xPBS)
 
-volume_final = 12  # Final volume of the diluted solution in mL
+      volume_final = 12  # Final volume of the diluted solution in mL
 
-# Calculate the volumes needed using the c1v1 formula
-volume_stock_formaldehyde = c1v1(c1=formaldehyde_stock, v1=None, c2=formaldehyde_dilution, v2=volume_final)
-volume_stock_pbs = c1v1(c1=pbs_stock, v1=None, c2=pbs_dilution, v2=volume_final)
-volume_water = volume_final - (volume_stock_formaldehyde + volume_stock_pbs)
+      # Calculate the volumes needed using the c1v1 formula
+      volume_stock_formaldehyde = c1v1(c1=formaldehyde_stock, v1=None, c2=formaldehyde_dilution, v2=volume_final)
+      volume_stock_pbs = c1v1(c1=pbs_stock, v1=None, c2=pbs_dilution, v2=volume_final)
+      volume_water = volume_final - (volume_stock_formaldehyde + volume_stock_pbs)
 
-# Display the results
-print(f"Total volume of the final solution: {volume_final} mL.")
-print(f"Volume of formaldehyde: {volume_stock_formaldehyde:.2f} mL.")
-print(f"Volume of 10xPBS: {volume_stock_pbs:.2f} mL.")
-print(f"Volume of water: {volume_water:.2f} mL.")
-print(f"Final concentrations: Formaldehyde: {formaldehyde_dilution}%, PBS: {pbs_dilution}X")
+      print(f"Volume of 10% formaldehyde: {volume_stock_formaldehyde:.2f} mL.")
+      print(f"Volume of 10xPBS: {volume_stock_pbs:.2f} mL.")
+      print(f"Volume of water: {volume_water:.2f} mL.")
+      print(f"Final concentrations: Formaldehyde: {formaldehyde_dilution}%, PBS: {pbs_dilution}X")
+
+
+if __name__ == "__main__":
+    formaldehyde()
